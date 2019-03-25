b7eefcd32ec974b94a022742a89d275591c4d61f
Conor Owens <conorj.owens@gmail.com>
2019-03-25 02:28:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,11 @@ def line(array)
   if array.size == 0
     puts "The line is currently empty."
   else 
+    lineup = []
     array.each_with_index do |x, i|
-      puts "The line is currently: #{i+1}. #{x}"
+      y = "#{i +1}. #{x}"
+      lineup << y
     end
+    puts lineup
   end
 end
