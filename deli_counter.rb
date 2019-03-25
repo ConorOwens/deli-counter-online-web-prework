1cd87904f9105ce8c10c447bdd4595577778cb73
Conor Owens <conorj.owens@gmail.com>
2019-03-25 02:16:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,21 +1,8 @@
-def lineup(array)
-   customers = []
-   array.each_with_index do |x, i|
-     y = "#{i + 1}. #{x}"
-     customers << y
-   end
-   return customers
- end
-   
-     
-  
 def line(array)
   if array.size == 0
     puts "The line is currently empty."
   else 
-    puts "The line is currently:"
-    lineup(array).each do |x|
-      puts x
+    
     end
   end
 end
