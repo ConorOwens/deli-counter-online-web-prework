44ad8c546cfda6e2db9eb1c1ae19aebb3e9f21c0
Conor Owens <conorj.owens@gmail.com>
2019-03-25 03:05:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -27,7 +27,7 @@ def now_serving(katz_deli)
   if katz_deli.size == 0 
     puts "There is nobody waiting to be served!"
   else
-    puts "Now serving"
+    puts "Currently serving #{katz_deli"
     katz_deli.shift
   end
 end
