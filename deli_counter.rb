1274d64697160b70335a043c4f5ee3b475f9e317
Conor Owens <conorj.owens@gmail.com>
2019-03-25 02:48:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,18 @@
-def line(array)
+katz_deli = []
+
+def line(katz_deli)
   if array.size == 0
     puts "The line is currently empty."
   else 
     lineup = []
-    array.each_with_index do |x, i|
+    katz_deli.each_with_index do |x, i|
       y = "#{i +1}. #{x}"
       lineup << y
     end
     puts "The line is currently: #{lineup.join(" ")}"
   end
+end
+
+def take_a_number(customer)
+  katz_deli << customer
 end
