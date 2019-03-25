4aaa70e80384ceaa7e13c546d39ec8ed172c6c84
Conor Owens <conorj.owens@gmail.com>
2019-03-25 01:48:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+
+def line(array)
+  if array.size == 0
+    puts "The line is currently empty."
+  else
+    puts "The line is currently: #{array.each_with_index(array) do |x, i|
+  end
+  
+end
